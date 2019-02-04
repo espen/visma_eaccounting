49d98f5fa1214465567ab5ab132137a145ba202e
Espen Antonsen <espen@inspired.no>
2019-02-04 16:12:54 +0100

    add missing api_environment and api_endpoint atributes

--- lib/visma_eaccounting/api_request.rb
+++ lib/visma_eaccounting/api_request.rb
@@ -78,6 +78,14 @@ module VismaEaccounting
       @request_builder.token
     end
 
+    def api_environment
+      @request_builder.api_environment
+    end
+
+    def api_endpoint
+      @request_builder.api_endpoint
+    end
+
     def timeout
       @request_builder.timeout
     end
